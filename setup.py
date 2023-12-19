bb2251ce5b64086a1db3c7068385e673219e4f22
Arslan Khalid <rj.arslan750@gmail.com>
2023-12-19 19:17:26 +0500

    Setting up

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Neural Networks made from scratch'
+LONG_DESCRIPTION = 'A package that can be used to make a neural network.'
+
+# Setting up
+setup(
+    name="NeuroPy",
+    version=VERSION,
+    author="Arslan Khalid",
+    author_email="iarslankhalid@yahoo.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(exclude=["tests", "docs"]),
+    install_requires=['numpy'],
+    keywords=['python', 'neural network', 'MLP', 'classification'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Users",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    url="https://github.com/iarslankhalid/NeuroPy",
+    project_urls={
+        "Source": "https://github.com/iarslankhalid/NeuroPy",
+        "Bug Reports": "https://github.com/iarslankhalid/NeuroPy/issues",
+    },
+    python_requires=">=3.6",
+)
